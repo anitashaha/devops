4bfe2cb1eca128e03d049898d1b17eeab5a0a766
anita <arav12378@gmail.com>
2021-01-17 16:34:39 -0600

    variable code

--- main.tf
+++ main.tf
@@ -9,10 +9,16 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0be2609ba883822ec"
-  instance_type = "t2.micro"
+  ami = "ami-0be2609ba883822ec"
+  instance_type = var.instance_type
+  
+}
+
+variable "instance_type" {
+  type = string
+    
 }
