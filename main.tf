f38b861c32dca706af75c9f096f1d0cca93e72ec
anita <arav12378@gmail.com>
2021-01-21 11:25:46 -0600

    dev2020

--- main.tf
+++ main.tf
@@ -15,7 +15,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0be2609ba883822ec"
   instance_type = var.instance_type
-  
+  key_name = "dev2020"
+  subnet_id = "subnet-00b43866"
+  security_groups = ["sg-0a3b2bd643d319f09"] 
 }
 
 variable "instance_type" {
